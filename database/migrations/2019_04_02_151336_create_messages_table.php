58bf122637fd2c7916a5f432c29f8a4abc2f4f99
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2019-04-06 00:14:17 +0500

    Make message model, migration and make relation with user model. Add chat controller, view and route.

--- database/migrations/2019_04_02_151336_create_messages_table.php
+++ database/migrations/2019_04_02_151336_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 }
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Chats</div>
+                <div class="card-body">
+                   Chats
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card">
+                <div class="card-header">Users</div>
+                <div class="card-body">
+                   Users
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
